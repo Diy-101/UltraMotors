bfa2114c956fcdc65437a536de70b024336a2b84
Diy-101 <thehorsediy101@gmail.com>
2025-06-30 21:03:18 +0300

    update brands.ts

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -1,6 +1,11 @@
 import type { Brand } from "../types/brand";
 import KamazLogo from "../assets/logos/kamaz.svg";
-import VolvoLogo from "@/assets/logos/volvo.svg";
+import CaterpillarLogo from "../assets/logos/caterpillar.svg";
+import ManLogo from "../assets/logos/man.svg";
+import LiebherrLogo from "../assets/logos/Liebherr.svg";
+import ScaniaLogo from "../assets/logos/scania.svg";
+import VolvoLogo from "../assets/logos/volvo.svg";
+import PonsseLogo from "../assets/logos/Ponsse.svg";
 
 export const brands: Brand[] = [
   {
@@ -12,5 +17,30 @@ export const brands: Brand[] = [
     name: "Volvo",
     logo: VolvoLogo,
     url: "https://volvo.com",
+  },
+  {
+    name: "Caterpillar",
+    logo: CaterpillarLogo,
+    url: "https://kamaz.ru",
+  },
+  {
+    name: "Man",
+    logo: ManLogo,
+    url: "https://volvo.com",
+  },
+  {
+    name: "Liebherr",
+    logo: LiebherrLogo,
+    url: "https://kamaz.ru",
+  },
+  {
+    name: "Scania",
+    logo: ScaniaLogo,
+    url: "https://volvo.com",
+  },
+  {
+    name: "Ponsse",
+    logo: PonsseLogo,
+    url: "https://kamaz.ru",
   },
 ];
