bc3b38dabb311a159c3faa3e94401f4e10fbbdc4
Diy-101 <thehorsediy101@gmail.com>
2025-07-14 18:30:42 +0300

    combine 2 functions

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from uvicorn.config import LOGGING_CONFIG
 
 # SMTP & SSL & OS
 from smtplib import SMTP_SSL
@@ -20,7 +21,7 @@ load_dotenv("/var/www/UltraMotors/backend/.env")
 
 EMAIL_LOGIN = os.getenv("EMAIL_LOGIN")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-
+WORKING_EMAIL = "6801782@mail.ru"
 # Create all tables from models.py in db
 Base.metadata.create_all(bind=engine)
 
@@ -58,112 +59,66 @@ def get_db():
         db.close()
 
 
-def send_email(contact:schemas.ContactCreate, server: str = 'smtp.yandex.ru', port: int = 465) -> bool:
+def send_email(contact: dict, server: str = 'smtp.yandex.ru', port: int = 465) -> bool:
     """
-    The function `send_email` sends an email to a contact using SMTP with optional server and port
-    parameters, handling exceptions and returning a boolean based on success.
+    The function `send_email` sends an email to a customer and the owner of a company with specific
+    content and details.
 
     Args:
-      contact (schemas.ContactCreate): The `send_email` function takes in a `contact` parameter of type
-    `schemas.ContactCreate`, which likely contains information about the recipient of the email such as
-    their email address, name, etc. The function also has optional parameters `server` and `port` which
-    default to 'smtp.yandex
-      server (str): The `server` parameter in the `send_email` function is used to specify the SMTP
-    server address to which the email will be sent. In this case, the default SMTP server address is set
-    to 'smtp.yandex.ru'. You can change this server address to the one provided by your email service.
-    Defaults to smtp.yandex.ru
-      port (int): Port is the port number used for the SMTP server. In this case, the default port is
-    set to 465, which is commonly used for SMTP over SSL (SMTPS). Defaults to 465
+      contact (dict): The `contact` parameter in the `send_email` function is a dictionary containing
+    information about the recipient of the email. It typically includes the following keys: name, email, phone, message
+      server (str): The `server` parameter in the `send_email` function represents the SMTP server
+    address that will be used to send the email. In this case, the default SMTP server address is set to
+    'smtp.yandex.ru'.SMTP (Simple Mail Transfer Protocol) is a communication protocol for email
+    transmission. Defaults to smtp.yandex.ru
+      port (int): Port is the parameter that specifies the port number for the email server. In this
+    case, the default port number is set to 465 for the SMTP server 'smtp.yandex.ru'. Defaults to 465
 
     Returns:
-      The `send_email` function returns a boolean value - `True` if the email was successfully sent, and
-    `False` if there was an error during the sending process.
+      The function `send_email` returns a boolean value. It returns `True` if the email was successfully
+    sent to the client and the owner, and it returns `False` if there was an error during the email
+    sending process.
     """
 
     if not EMAIL_LOGIN or not EMAIL_PASSWORD:
         raise ValueError('You should make .env file with following variables:\nEMAIL_LOGIN\nEMAIL_PASSWORD')
-
-    contact_info: dict = contact.model_dump()
 
     text_subtype = 'plain'
     content = """\
         Text
         """
 
-    msg = MIMEText(content, text_subtype, 'utf-8')
-    msg['Subject'] = "Компания «UltraMotors» приветствует вас"
-    msg['From'] = f"UltraMotors <{EMAIL_LOGIN}>"
-    msg['To'] = contact_info['email']
+    msg_to_customer = MIMEText(content, text_subtype, 'utf-8')
+    msg_to_customer['Subject'] = "Компания «UltraMotors» приветствует вас"
+    msg_to_customer['From'] = f"UltraMotors <{EMAIL_LOGIN}>"
+    msg_to_customer['To'] = contact['email']
 
-    try:
-        context = ssl.create_default_context()
-        with SMTP_SSL(server, port, context=context) as sr:
-            sr.login(EMAIL_LOGIN, EMAIL_PASSWORD)
-            sr.sendmail(EMAIL_LOGIN, [contact_info['email']], msg.as_string())
-            print("✅ Письмо успешно отправлено клиенту")
-        return True
-    except Exception as e:
-        print(f"❌ Ошибка: {e}")
-        print('Тип:', type(e))
-        return False
-
-def send_self_email(contact:schemas.ContactCreate, server: str = 'smtp.yandex.ru', port: int = 465) -> bool:
-    """
-    The function `send_self_email` sends an email with contact information to a specified email address
-    using SMTP protocol.
-
-    Args:
-      contact (schemas.ContactCreate): The `send_self_email` function takes a `contact` object of type
-    `schemas.ContactCreate` as input, along with optional parameters `server` and `port` for the SMTP
-    server details. The function composes an email message using the contact information provided, and
-    attempts to send the email using
-      server (str): The `server` parameter in the `send_self_email` function represents the SMTP server
-    address that will be used to send the email. In this case, the default SMTP server address is set to
-    'smtp.yandex.ru'. This is the server that will handle the outgoing email communication. Defaults to
-    smtp.yandex.ru
-      port (int): The `port` parameter in the `send_self_email` function is used to specify the port
-    number for the SMTP server connection. In this case, the default port is set to `465`, which is
-    commonly used for SMTP over SSL (SMTPS) connections. This port is typically used for secure.
-    Defaults to 465
-
-    Returns:
-      The function `send_self_email` returns a boolean value - `True` if the email was successfully
-    sent, and `False` if there was an error during the sending process.
-    """
-
-    if not EMAIL_LOGIN or not EMAIL_PASSWORD:
-        raise ValueError('You should make .env file with following variables:\nEMAIL_LOGIN\nEMAIL_PASSWORD')
-
-    contact_info: dict = contact.model_dump()
-
-    text_subtype = 'plain'
-    content = f"""\
+    msg_to_owner = MIMEText(f"""\
         На сайте клиент прошел форму для связи и ожидает вашего звонка. Вот его данные:\n
-        Имя: {contact_info['first_name']}\n\
-        Телефон: {contact_info['phone']}\n\
-        Email: {contact_info['email']}\n\
-        Сообщение от клиента: {contact_info['message']}
-        """
+        Имя: {contact['first_name']}\n\
+        Телефон: {contact['phone']}\n\
+        Email: {contact['email']}\n\
+        Сообщение от клиента: {contact['message']}
+        """, text_subtype, "utf-8")
 
-    msg = MIMEText(content, text_subtype, 'utf-8')
-    msg['Subject'] = "Компания «UltraMotors» приветствует вас"
-    msg['From'] = f"UltraMotors <{EMAIL_LOGIN}>"
-    msg['To'] = "6801782@mail.ru"
+    msg_to_owner['Subject'] = "Компания «UltraMotors» приветствует вас"
+    msg_to_owner['From'] = f"UltraMotors <{EMAIL_LOGIN}>"
+    msg_to_owner['To'] = WORKING_EMAIL
 
     try:
         context = ssl.create_default_context()
         with SMTP_SSL(server, port, context=context) as sr:
             sr.login(EMAIL_LOGIN, EMAIL_PASSWORD)
-            sr.sendmail(EMAIL_LOGIN, ["6801782@mail.ru"], msg.as_string())
-            print('Message sent')
-
-        print("✅ Письмо успешно отправлено на 6801782@mail.ru!")
+            sr.sendmail(EMAIL_LOGIN, [contact['email']], msg_to_customer.as_string())
+            sr.sendmail(EMAIL_LOGIN, WORKING_EMAIL, msg_to_owner.as_string())
+            print("✅ Письмо успешно отправлено клиенту и владельцу")
         return True
     except Exception as e:
         print(f"❌ Ошибка: {e}")
         print('Тип:', type(e))
         return False
 
+# Endpoints
 @app.post("/api/contact")
 def submit_contact(
     contact: schemas.ContactCreate,
@@ -184,8 +139,6 @@ def submit_contact(
         models.Contact: The newly created contact
     """
 
-
-    send_self_email(contact)
-    send_email(contact)
+    send_email(contact.model_dump())
 
     return crud.create_contact(db, contact)
